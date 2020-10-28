a2b7cab35ac1505838dd78a91ecf4a8dccec2ab1
yang <anyang47@hotmail.com>
2020-10-28 15:10:55 -0400

    implement product index and show, add admin dashboard link to nav

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to:'admin/products#index'
+  # root to:'admin/products#index'
+  root to: 'products#index'
   namespace :admin do
     resources :products
   end
-  get 'welcome', to: 'welcome#index'
+  resources :products
+  # get 'welcome', to: 'welcome#index'
 end
