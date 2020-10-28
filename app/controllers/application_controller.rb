3b4ba2eccf2b1250227627ca22564cb90233152b
yang <anyang47@hotmail.com>
2020-10-28 17:17:48 -0400

    implement add to cart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,23 @@ class ApplicationController < ActionController::Base
             redirect_to new_user_session_path
         end
     end
+
+    # For view
+    helper_method :current_cart
+
+    def current_cart
+        @current_cart ||= find_cart
+    end
+
+
+    private
+
+    def find_cart
+        cart = Cart.find_by(id: session[:cart_id])
+        if cart.blank?
+            cart = Cart.create
+        end
+        session[:cart_id] = cart.id
+        return cart
+    end
 end
